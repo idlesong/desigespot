7295e6df5eed8e3ddb5110bbd8b58ee3a4c277b7
idlesong <idlesong@gmail.com>
2015-01-30 15:55:52 +0800

    heroku test

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,4 +5,11 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+# Rails.application.config.assets.precompile += %w( activeaxon_menu.js, animationEnigne.js, bootstrap.min.js,
+# ie-fixes.js, jq.appear.js, jquery.base64.js, jquery.carouFredSel-6.2.1-packed.js, jquery.cycle.js,
+# jquery.cycle2.carousel.js, jquery.easing.1.3.js, jquery.easytabs.js, jquery.infinitescroll.js,
+# jquery.isotope.js, jquery.prettyPhoto.js, jQuery.scrollPoint.js, jquery.themepunch.plugins.min.js,
+# jquery.themepunch.revolution.js, jquery.tipsy.js, jquery.validate.js, jQuery.XDomainRequest.js,
+# kanzi.js, respond.js, retina.js,
+# _colorpicker.js, _handlebars.js, _jq.js, _jquery.placeholder.js, _scripts.js, _skinchooser.js, _skins.js,
+# application.js )
